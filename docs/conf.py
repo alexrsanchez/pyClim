d6e6935b19510c7e08774a967ec927eda245295d
alexrsanchez <72650160+alexrsanchez@users.noreply.github.com>
2026-02-15 20:59:23 +0100

    first commit pyClim v1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pyclim'
-copyright = '2026, Alejandro Rodríguez Sánchez'
-author = 'Alejandro Rodríguez Sánchez'
-release = '1.0.1'
+project = "pyclim"
+copyright = "2026, Alejandro Rodríguez Sánchez"
+author = "Alejandro Rodríguez Sánchez"
+release = "1.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
